deae612aecaa52b9789d1a5e9a82e043a70ad278
b077vg <57477947+b077vg@users.noreply.github.com>
2020-09-08 01:28:20 +0900

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,35 @@
+let w=300,h=300; //not scale 200 
+let img=[];
+let m;
+let angle=0;
+let q=51;
+let mn=51;
+let mx=150;
+let z=0;
+function setup() {  
+	createCanvas(w,h);
+	for(let i=0;i<100;i++){
+		img[i] = loadImage("data/picture1/Movie"+q+".jpg");
+		q++;
+	}
+}
+
+function draw() {
+  background(200);
+  if(mouseX>0 && mouseX<w){
+  	m=map(mouseX,z,w,mn,mx);
+	m=Math.round(m);
+  }
+	console.log(m);
+  createTarget();
+}
+
+function createTarget(){
+	for(let i=1; i<100; i++){
+		if(m==(i+50)){
+		push();
+			image(img[100-i], 0, 0,w,h); 
+		pop();
+		}
+	}
+}
